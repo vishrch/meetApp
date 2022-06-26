070ea01804a24d8d5439abf9cd86df3b5beb8c71
Vishnu R Chandran <vishrch@gmail.com>
2022-06-26 22:35:18 +0530

    package

--- App.tsx
+++ App.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
 import { View, LogBox, StatusBar } from 'react-native';
 import AppWithNavigationState from 'navigators';
-import { } from 'components';
+import {} from 'components';
 import GlobalLoading, { globalLoadingRef } from 'components/GlobalLoading';
 import GlobalMessage, { globalMessageRef } from 'components/GlobalMessage';
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'shared/language';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { NavigationContainer } from '@react-navigation/native';
+import { ApplicationProvider } from '@ui-kitten/components';
 import {
   globalCallRef,
   GlobalCallUI,
   globalGroupCallRef,
   GlobalGroupCallUI,
 } from './src/simple-master/UIKit';
+import { useEffect } from 'react';
+import { DeviceEventEmitter, Platform } from 'react-native';
+import IncomingCall from 'react-native-incoming-call';
+import messaging from '@react-native-firebase/messaging';
 
 LogBox.ignoreAllLogs(true);
 StatusBar.setBarStyle('dark-content');
 const MyBase = () => {
+  useEffect(() => {
+    incomingCall();
+    requestUserPermission();
+  }, []);
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+    }
+  };
+
+  const incomingCall = async () => {
+    if (Platform.OS === 'android') {
+      const payload = await IncomingCall.getExtrasFromHeadlessMode();
+      console.log('launchParameters', payload);
+      if (payload) {
+      }
+      DeviceEventEmitter.addListener('endCall', payload => {});
+      DeviceEventEmitter.addListener('answerCall', payload => {});
+    }
+  };
+
   return (
     <View style={{ flex: 1 }}>
       <I18nextProvider i18n={i18next}>
