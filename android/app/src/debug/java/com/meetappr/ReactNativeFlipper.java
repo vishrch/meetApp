7205b2ca4551ef01b3aef873611a3d1b7f4fe3f8
Vishnu R Chandran <vishrch@gmail.com>
2022-06-27 00:40:00 +0530

    wip

--- android/app/src/debug/java/com/meetappr/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/meetappr/ReactNativeFlipper.java
@@ -40,32 +40,32 @@ public class ReactNativeFlipper {
           new NetworkingModule.CustomClientBuilder() {
             @Override
             public void apply(OkHttpClient.Builder builder) {
-              builder.addNetworkInterceptor(new FlipperOkhttpInterceptor(networkFlipperPlugin));
+              // builder.addNetworkInterceptor(new FlipperOkhttpInterceptor(networkFlipperPlugin));
             }
           });
-      client.addPlugin(networkFlipperPlugin);
-      client.start();
+      // client.addPlugin(networkFlipperPlugin);
+      // client.start();
 
       // Fresco Plugin needs to ensure that ImagePipelineFactory is initialized
       // Hence we run if after all native modules have been initialized
       ReactContext reactContext = reactInstanceManager.getCurrentReactContext();
       if (reactContext == null) {
-        reactInstanceManager.addReactInstanceEventListener(
-            new ReactInstanceManager.ReactInstanceEventListener() {
-              @Override
-              public void onReactContextInitialized(ReactContext reactContext) {
-                reactInstanceManager.removeReactInstanceEventListener(this);
-                reactContext.runOnNativeModulesQueueThread(
-                    new Runnable() {
-                      @Override
-                      public void run() {
-                        client.addPlugin(new FrescoFlipperPlugin());
-                      }
-                    });
-              }
-            });
+        // reactInstanceManager.addReactInstanceEventListener(
+        //     new ReactInstanceManager.ReactInstanceEventListener() {
+        //       @Override
+        //       public void onReactContextInitialized(ReactContext reactContext) {
+        //         reactInstanceManager.removeReactInstanceEventListener(this);
+        //         reactContext.runOnNativeModulesQueueThread(
+        //             new Runnable() {
+        //               @Override
+        //               public void run() {
+        //                 client.addPlugin(new FrescoFlipperPlugin());
+        //               }
+        //             });
+        //       }
+        //     });
       } else {
-        client.addPlugin(new FrescoFlipperPlugin());
+        // client.addPlugin(new FrescoFlipperPlugin());
       }
     }
   }
--- android/app/src/main/java/com/meetappr/MainActivity.java
+++ android/app/src/main/java/com/meetappr/MainActivity.java
@@ -1,6 +1,19 @@
 package com.meetappr;
 
 import com.facebook.react.ReactActivity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import androidx.annotation.RequiresApi;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +25,13 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "MeetAppR";
   }
+
+    public static PendingIntent getActionIntent(int notificationId, Uri uri, Context context) {
+        // Intent intent =  new Intent(Intent.ACTION_VIEW,uri);
+        // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // intent.putExtra(NOTIFICATION_ID, notificationId);
+        // PendingIntent acceptIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        return null;
+    }
+
 }
